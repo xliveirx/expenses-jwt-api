d791f35638ea68e7a7f4913b6718d485601e7580
João Oliveira <xliveirvwrld@gmail.com>
2025-07-30 17:48:44 -0300

    feat: initial commit

---
+++ src/main/java/br/com/joao/api_despesas/despesas/DespesasApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.api_despesas.despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joao/api_despesas/despesas/DespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.api_despesas.despesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

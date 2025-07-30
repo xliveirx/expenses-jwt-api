6da0eae3e66d5bed3320bae30512d542dd213696
João Oliveira <xliveirvwrld@gmail.com>
2025-07-30 17:49:49 -0300

    feat: initial commit

---
+++ src/main/java/br/com/joao/api_despesas/despesas/domain/user/User.java
@@ -0,0 +1,58 @@
+package br.com.joao.api_despesas.despesas.domain.user;
+
+import br.com.joao.api_despesas.despesas.dto.UserRequestDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean enabled;
+
+    public User(UserRequestDTO dto, String password) {
+        this.name = dto.name();
+        this.email = dto.email();
+        this.username = dto.username();
+        this.password = password;
+        this.enabled = true;
+        this.role = Role.USER;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
